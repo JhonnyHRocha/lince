78ceb13067f9487006fd79d5cb6d9e2676205ef1
JhonnyHRocha <jonathan@jhrdesenvolvimentos.com>
2015-10-22 11:00:03 -0200

    Dependencias do Gulpfile
    Dependencias base do gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,38 @@
-var elixir = require('laravel-elixir');
+var elixir = require('laravel-elixir'),
+    liveReload = require('gulp-livereload'),
+    clean = require('gulp-clean'),
+    gulp = require('gulp');
+
+
+var config = {
+    assets_path: './resources/assets',
+    build_path: './public/build'
+};
+
+config.bower_path = config.assets_path + '/../bower_components';
+
+//CAMINHO DOS ARQUIVOS DE JAVASCRIPT E JAVASCRIPT DE TERCEIROS QUE VAO FICAR NA PASTA PUBLIC
+config.build_path_js = config.build_path + '/js';
+config.build_vendor_path_js = config.build_path_js + '/vendor';
+config.build_path_js = [
+    config.bower_path + '/jquery/dist/jquery.min.js',
+    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+    config.bower_path + '/angular/angular.min.js',
+    config.bower_path + '/angular-route/angular-route.min.js',
+    config.bower_path + '/angular-resource/angular-resource.min.js',
+    config.bower_path + '/angular-animate/angular-animate.min.js',
+    config.bower_path + '/angular-messages/angular-messages.min.js',
+    config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+    config.bower_path + '/angular-strap/modules/navbar.min.js',
+];
+
+//CAMINHO DOS ARQUIVOS DE CSS E CSS DE TERCEIROS QUE VAO FICAR NA PASTA PUBLIC
+config.build_path_css = config.build_path + '/css';
+config.build_vendor_path_css = config.build_path_css + '/vendor';
+config.build_path_css = [
+    config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
+    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+];
 
 /*
  |--------------------------------------------------------------------------

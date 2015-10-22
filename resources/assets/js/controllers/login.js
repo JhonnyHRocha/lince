b0a628d5a0410b605784d71eee4b85de720a36aa
JhonnyHRocha <jonathan@jhrdesenvolvimentos.com>
2015-10-22 17:15:52 -0200

    Login 2
    Tratando erros no momento de fazer o login no sistema

--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -1,16 +1,22 @@
 angular.module('app.controllers')
 .controller('LoginController', ['$scope','$location', 'OAuth',function($scope,$location,OAuth){
-        $scope.user ={
+        $scope.user = {
             username: '',
             password: ''
+        };
+
+        $scope.error = {
+            message: '',
+            error: false
         };
 
         $scope.login = function(){
             if($scope.form.$valid){
                 OAuth.getAccessToken($scope.user).then(function() {
                     $location.path('home');
-                }, function(){
-                    alert('Login inválido');
+                }, function(data){
+                    $scope.error.error = true;
+                    $scope.error.message = data.data.error_description;
                 });
             }
 

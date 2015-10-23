3778de4acaa48748d9f23d9592865ba223f7a8c5
JhonnyHRocha <jonathan@jhrdesenvolvimentos.com>
2015-10-23 13:17:00 -0200

    Task CSS Theme + arquivos

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,26 @@ config.vendor_path_css = [
 //CAMINHO DOS ARQUIVOS DE HTML E HTML DE TERCEIROS QUE VAO FICAR NA PASTA PUBLIC
 config.build_path_html = config.build_path + '/views';
 
+config.build_path_font = config.build_path + '/fonts';
+config.build_path_images = config.build_path + '/images';
+
+
+gulp.task('copy-font', function () {
+    gulp.src([
+        config.assets_path + '/fonts/**/*'
+    ])
+        .pipe(gulp.dest(config.build_path_font))
+        .pipe(liveReload());
+});
+
+gulp.task('copy-image', function () {
+    gulp.src([
+        config.assets_path + '/img/**/*'
+    ])
+        .pipe(gulp.dest(config.build_path_images))
+        .pipe(liveReload());
+});
+
 gulp.task('copy-html', function () {
     gulp.src([
         config.assets_path + '/js/views/**/*.html'
@@ -80,7 +100,7 @@ gulp.task('clear-build-folder', function(){
 });
 
 gulp.task('default', ['clear-build-folder'], function(){
-    gulp.start('copy-html')
+    gulp.start('copy-html', 'copy-font', 'copy-image');
     elixir(function(mix){
         mix.styles(config.vendor_path_css.concat([config.assets_path + '/css/**/*.css']),
         'public/css/all.css', config.assets_path);
@@ -92,6 +112,6 @@ gulp.task('default', ['clear-build-folder'], function(){
 
 gulp.task('watch-dev', ['clear-build-folder'], function(){
     liveReload.listen();
-    gulp.start('copy-styles', 'copy-scripts', 'copy-html');
+    gulp.start('copy-styles', 'copy-scripts', 'copy-html', 'copy-font', 'copy-image');
     gulp.watch(config.assets_path + '/**', ['copy-styles', 'copy-scripts', 'copy-html']);
 });

05b550d9d787cf1067d675f04c50f9f66251a40a
JhonnyHRocha <jonathan@jhrdesenvolvimentos.com>
2015-10-31 14:10:09 -0200

    Form Usuario - Campo validade Ok
    Campo referente a data de validade do usuário está funcionando
    corretamente.

--- resources/assets/js/services/usuarioService.js
+++ resources/assets/js/services/usuarioService.js
@@ -4,12 +4,13 @@ angular.module('app.services')
         function transformData(data){
             if(angular.isObject(data) && data.hasOwnProperty('password')){
                 var o = angular.copy(data);
-
                 if(angular.isObject(data) && data.hasOwnProperty('data_validade')){
-                    o.data_validade = $filter('date')(o.data_validade, 'yyyy-MM-dd');
+                    var dateArray = data.data_validade.split('/'),
+                        month = dateArray[1] - 1;
+                    o.data_validade = new Date(dateArray[2],month,dateArray[0]);
+                    //o.data_validade = $filter('date')(o.data_validade, 'yyyy-MM-dd');
                     return appConfig.utils.transformRequest(o);
                 }
-
                 return appConfig.utils.transformRequest(o);
             }
             return data;
@@ -25,7 +26,15 @@ angular.module('app.services')
             },
             get: {
                 method: 'GET',
-                isArray: false
+                isArray: false,
+                transformResponse: function(data, headers){
+                    var o = appConfig.utils.transformResponse(data,headers);
+                    if(angular.isObject(o) && o.hasOwnProperty('data_validade')){
+                        o.data_validade = $filter('date')(o.data_validade, 'dd/MM/yyyy');
+                    }
+                    return o;
+                }
+
             }
         });
     }]);
